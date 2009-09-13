2fc9998b907f7f2071d57da440b3d593786b2527
Andreas Knifh <knifhster@gmail.com>
2009-09-13 17:15:39 +0200

    refactored all authentication related methods from ApiController into AuthenticationController

--- screenbook-service/grails-app/controllers/ApiController.groovy
+++ screenbook-service/grails-app/controllers/ApiController.groovy
@@ -8,9 +8,7 @@ class ApiController {
 	def accountService
 	def answerService
 
-    def beforeInterceptor = [action:this.&apiAuthentication,except:['createMainAccount', 'verifyLogin', 'loginAsTeacher', 'verifyApiLogin', 'loginAsStudent']]
-
-    def apiAuthentication =  {
+    def beforeInterceptor = {
       def authId = params.id
       def authApikey = params.apikey
       if (!accountService.verifyApiLogin(authId, authApikey)) {
@@ -18,83 +16,6 @@ class ApiController {
         return false
       }
     }
-
-	def createMainAccount = {
-      def account = accountService.createMainAccount(params.username)
-
-      render account as XML
-    }
-
-	def verifyLogin = {
-		def username = params.username;
-		def password = params.password;
-        def result = false
-        try {
-          result = accountService.verifyLogin(username, password) != null
-        } catch (UserNotFoundException e) {
-        } catch (IncorrectPasswordException) {}
-		
-        render result as XML
-	}
-
-    def loginAsTeacher = {
-      def username = params.username
-      def password = params.password
-
-      def apikey
-      def result
-      def error
-      try {
-        apikey = accountService.verifyLogin(username, password)
-        result ="success"
-      } catch (UserNotFoundException e) {
-        result = "failure"
-        error = e.message
-      } catch (IncorrectPasswordException e) {
-        result = "failure"
-        error = e.message
-      }
-
-      render text: ApiResults.getLoginAsTeacherResult(username, apikey, result, error), contentType:"text/xml"
-    }
-
-    def verifyApiLogin = {
-      def username = params.username
-      def apikey = params.apikey
-
-      render accountService.verifyApiLogin(username, apikey) as XML
-    }
-
-	def loginAsStudent = {
-		def mainAccountName = params.mainAccountName
-		def studentAccountName = params.studentAccountName
-
-        if (mainAccountName == null) {
-            log.warn("Main account name is empty");
-            return;
-        }
-
-        if (studentAccountName == null) {
-            log.warn("Student account name is empty");
-            return;
-        }
-
-        if (!accountService.mainAccountExists(mainAccountName)) {
-            log.warn("Could not find main account '${mainAccountName}'");
-            render false as XML;
-            return; 
-        }
-
-		def loginVerified = accountService.verifyStudentLogin(mainAccountName, studentAccountName)
-		if (!loginVerified && accountService.verifyFreeLicences(mainAccountName)) {
-			accountService.createStudentAccount(mainAccountName, studentAccountName)
-		} else if (!loginVerified) {
-			render false as XML
-			return
-		}
-
-		render true as XML
-	}
 
 	/**
      * @param username
