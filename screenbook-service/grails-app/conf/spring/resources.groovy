538edcb993853ddd3e7a5c6fcdc97033619659af
Andreas Knifh <knifhster@gmail.com>
2009-09-27 18:25:16 +0200

    using database connection pool

--- screenbook-service/grails-app/conf/spring/resources.groovy
+++ screenbook-service/grails-app/conf/spring/resources.groovy
@@ -1,6 +1,25 @@
+import grails.util.Environment
+
 // Place your Spring DSL code here
 beans = {
 		authenticationEntryPoint(org.springframework.security.ui.basicauth.BasicProcessingFilterEntryPoint) {
 		    realmName = 'Screenbook service'
+		}
+
+        if ([Environment.DEVELOPMENT, Environment.PRODUCTION].contains(Environment.current)) {
+
+			println("Using overridden datasource from resources.groovy since the environment is ${Environment.current.name}.")
+
+			dataSource(org.apache.commons.dbcp.BasicDataSource) { bean ->
+				bean.destroyMethod = "close"
+
+				driverClassName="com.mysql.jdbc.Driver"
+				url="jdbc:mysql://localhost/screenbook-dev"
+				username = "screenbook-dev"
+				password = "screenbook-dev"
+				testOnBorrow = "true"
+				validationQuery = "SELECT 1 FROM DUAL;"
+				defaultAutoCommit = "false"
+			}
 		}
 }
