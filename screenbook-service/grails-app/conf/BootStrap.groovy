e1283aef02e2adf3c2178a5d2c94de61f264f4a0
J-M Bergqvist <jm.bergqvist@gmail.com>
2009-10-12 00:05:55 +0200

    Do account changes within transaction, otherwise it doesn't seem to work reliably.

--- screenbook-service/grails-app/conf/BootStrap.groovy
+++ screenbook-service/grails-app/conf/BootStrap.groovy
@@ -8,23 +8,26 @@ class BootStrap {
     /* Check or add roles. */
     ["ROLE_ADMIN", "ROLE_TEACHER"].each {
       if (!roleService.roleExists(it)) {
+        log.info("Adding role ${it}")
         roleService.addRole(it)
       }
     }
 
     /* Check or add admin account. */
     if (!accountService.mainAccountExists("admin")) {
-      log.info("Could not find admin account. Adding...")
-      def account = accountService.createAdminAccount("admin", "jm.bergqvist@gmail.com")
-      account.apikey = "a1984cf7-c2eb-400c-abe6-a1ee9e151ee1"
-      account.save(flush:true)
-      accountService.setNewPassword("admin", "admin")
 
-      new Book(name:"book1").save()
-            
+      Account.withTransaction { status ->
+        log.info("Could not find admin account. Adding...")
+        def account = accountService.createAdminAccount("admin", "jm.bergqvist@gmail.com")
+        log.info("Created account ${account}")
+
+        accountService.setNewPassword("admin", "admin")
+      }
+
     }
 
   }
+
   def destroy = {
   }
 }
