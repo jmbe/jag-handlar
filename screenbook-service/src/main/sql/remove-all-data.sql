a31cdc86196e8b1201ebdcdff6a978bbfb2aa410
J-M Bergqvist <jm.bergqvist@gmail.com>
2009-11-09 16:51:23 +0100

    Change delete order to avoid foreign key constraints problem.

--- screenbook-service/src/main/sql/remove-all-data.sql
+++ screenbook-service/src/main/sql/remove-all-data.sql
@@ -3,7 +3,7 @@ use `screenbook-dev`;
 delete from role_people;
 delete from role;
 delete from work_book;
+delete from purchase;
 delete from student;
 delete from account;
 delete from book;
-delete from purchase;
