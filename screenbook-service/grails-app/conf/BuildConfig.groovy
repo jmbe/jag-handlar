995d46d0c3296358317b2c228933b3eb01a229d0
J-M Bergqvist <jm.bergqvist@gmail.com>
2011-12-30 14:27:41 +0100

    Fix classpath issues with slf4j and grails 1.3.7.

--- screenbook-service/grails-app/conf/BuildConfig.groovy
+++ screenbook-service/grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,13 @@
 //grails.project.test.class.dir = "target/test-classes"
 //grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+
+grails.war.resources = { stagingDir ->
+    delete(file:"${stagingDir}/WEB-INF/lib/commons-logging-1.1.1.jar")
+}
+
+
 grails.project.dependency.resolution = {
     useOrigin true
 
@@ -37,7 +44,16 @@ grails.project.dependency.resolution = {
         compile "packtag:packtag:3.6"
         compile "com.yahoo.platform.yui:yuicompressor:2.3.6"
 
-        compile "se.pictosys:payment-web:2.0.1-SNAPSHOT"
+        compile ("se.pictosys:payment-web:2.0.1-SNAPSHOT") {
+            /* exclude only because it pulls in slf4j 1.6.4 which is not compatible with 
+             * Grails 1.3.7. Exclusion can be removed on upgrade. */
+            excludes ([group: "com.mysema.querydsl", artifact:"querydsl-jpa"])
+        }
         compile "se.pictosys:pictosys-web:2.0.1-SNAPSHOT"
+
+        compile "org.slf4j:jcl-over-slf4j:1.5.8"
+        compile "org.slf4j:jul-to-slf4j:1.5.8"
+        compile "org.slf4j:slf4j-api:1.5.8"
+        compile "org.slf4j:slf4j-log4j12:1.5.8"
     }
 }
