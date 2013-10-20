e6421e4851f9b970da2f447957f997514177dbc9
J-M Bergqvist <jm.bergqvist@gmail.com>
2013-10-20 18:38:23 +0200

    Format file.

--- screenbook-service/grails-app/controllers/se/jaghandlar/AuthenticationController.groovy
+++ screenbook-service/grails-app/controllers/se/jaghandlar/AuthenticationController.groovy
@@ -1,6 +1,6 @@
 package se.jaghandlar
 import grails.converters.XML
-import se.jaghandlar.Account;
+import se.jaghandlar.Account
 import se.jaghandlar.exceptions.UserNotFoundException
 import se.jaghandlar.exceptions.IncorrectPasswordException
 import se.jaghandlar.util.XmlResults
@@ -10,95 +10,94 @@ class AuthenticationController {
     def answerService
 
     def createMainAccount = {
-      def account = accountService.createMainAccount(params.username, params.email)
+        def account = accountService.createMainAccount(params.username, params.email)
 
-      render account as XML
+        render account as XML
     }
 
-	def verifyLogin = {
-		def username = params.username;
-		def password = params.password;
+    def verifyLogin = {
+        def username = params.username
+        def password = params.password
         def result = false
         try {
-          result = accountService.verifyLogin(username, password) != null
+            result = accountService.verifyLogin(username, password) != null
         } catch (UserNotFoundException e) {
-        } catch (IncorrectPasswordException) {}
+        } catch (IncorrectPasswordException) {
+        }
 
         render result as XML
-	}
+    }
 
     def loginAsTeacher = {
-      def username = params.username
-      def password = params.password
+        def username = params.username
+        def password = params.password
 
-      def apikey
-      def result = "failure"
-      def error
-      try {
-        /* Authentication */
-        apikey = accountService.verifyLogin(username, password)
+        def apikey
+        def result = "failure"
+        def error
+        try {
+            /* Authentication */
+            apikey = accountService.verifyLogin(username, password)
 
-        /* Authorization */
-        def account = Account.findByUsername(username)
-        if (account.isActive()) {
-          result = "success"
-        } else {
-          error = "error.account.expired"
+            /* Authorization */
+            def account = Account.findByUsername(username)
+            if (account.isActive()) {
+                result = "success"
+            } else {
+                error = "error.account.expired"
+            }
+        } catch (UserNotFoundException e) {
+            error = e.message
+        } catch (IncorrectPasswordException e) {
+            error = e.message
         }
-      } catch (UserNotFoundException e) {
-        error = e.message
-      } catch (IncorrectPasswordException e) {
-        error = e.message
-      }
 
-      render XmlResults.getLoginAsTeacherResult(username, apikey, result, error, false)
+        render XmlResults.getLoginAsTeacherResult(username, apikey, result, error, false)
     }
 
     def changeTeacherPassword = {
-      def accountName = params.account
-      def oldPassword = params.oldPassword
-      def newPassword = params.newPassword
+        def accountName = params.account
+        def oldPassword = params.oldPassword
+        def newPassword = params.newPassword
 
-      def result = accountService.changePassword(accountName, oldPassword, newPassword)
+        def result = accountService.changePassword(accountName, oldPassword, newPassword)
 
-      render result as XML
-
+        render result as XML
     }
 
     def verifyApiLogin = {
-      def username = params.username
-      def apikey = params.apikey
-      String error = "";
-      def active = false;
+        def username = params.username
+        def apikey = params.apikey
+        String error = ""
+        def active = false
 
-      def valid = accountService.verifyApiLogin(username, apikey);
+        def valid = accountService.verifyApiLogin(username, apikey)
 
-      if (valid) {
-        active = Account.findByUsername(username).isActive();
-        if (!active) {
-          error = "error.account.expired"
+        if (valid) {
+            active = Account.findByUsername(username).isActive()
+            if (!active) {
+                error = "error.account.expired"
+            }
         }
-      }
 
-      render new XmlResults().withVerifyApiLoginResult(valid && active, error).toXml();
+        render new XmlResults().withVerifyApiLoginResult(valid && active, error).toXml()
     }
 
 
     def lostPassword = {
-      def accountIdentifier = params.accountIdentifier
-      log.info("Received new password request for ${accountIdentifier}.")
-      render accountService.resetPassword(accountIdentifier) as XML
+        def accountIdentifier = params.accountIdentifier
+        log.info("Received new password request for ${accountIdentifier}.")
+        render accountService.resetPassword(accountIdentifier) as XML
     }
 
     def clearBookmarkReminder = {
-      def username = params.username
-      log.info "Clearing bookmark reminder for account ${username}"
+        def username = params.username
+        log.info "Clearing bookmark reminder for account ${username}"
 
 
 
-      accountService.clearBookmarkReminder(username)
+        accountService.clearBookmarkReminder(username)
 
-      render ""
+        render ""
     }
-
 }
